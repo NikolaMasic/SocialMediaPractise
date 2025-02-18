ea9feedc1a5cc404a9fa22d26ffd9f0c30b2d331
Nikola Masic <137811255+NikolaMasic@users.noreply.github.com>
2025-02-18 18:39:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+
+var database = [
+    {
+    username: 'Bob',
+    password: 'Secure',
+    },
+    {
+        username: 'Joel',
+        password: 'Yankies',
+        },
+        {
+            username: 'Marge',
+            password: '777',
+            },
+
+];
+var newsfeed = [ 
+     {
+        username: 'Bob',
+        timeline: 'So tired from yestarday!'
+     },
+     {  username: 'Susan',
+        timeline: 'JavaScript rules.',
+     },
+     {  username: 'Mike',
+        timeline: 'Look at my Avocado toast receipe that I found!'
+     }
+
+];
+
+function isUserValid(username, password){
+    for (var i=0; i < database.length; i++){
+        if(database[i].username === username &&
+            database[i].password === password){
+                return true;
+            }
+ }
+ return false;
+}
+
+function signIn(username, password){
+    if (isUserValid(username, password)) {
+            console.log(newsfeed);
+        } else {
+            alert('Sorry, wrong username/password');
+        }
+}
+var userNamePrompt = prompt('Username');
+var passwordPrompt = prompt('Password');
+
+signIn(userNamePrompt, passwordPrompt)
+
+
